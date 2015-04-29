327764f79d2aafb28fc7ea96759eaf54b1abe44e
Richard Lynch <rjlynch86@googlemail.com>
2015-04-29 14:00:12 +0100

    adds auto search functionality

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -19,7 +19,7 @@ describe('GitUserSearchController', function(){
     beforeEach(inject(function($httpBackend) {
       httpBackend = $httpBackend
       httpBackend
-      .when("GET", "https://api.github.com/search/users?access_token=027243cbc6861e2d7e31194da4cae9888cafd101&q-hello")
+      .when("GET", "https://api.github.com/search/users?access_token=027243cbc6861e2d7e31194da4cae9888cafd101&q=hello")
       .respond(
         { items: items }
         );
